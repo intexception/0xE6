114c65edc88a3691247f4dce0f491d8f0d46cc92
nquantum <80127180+intexception@users.noreply.github.com>
2021-08-01 19:26:03 +0200

    Add files via upload

---
+++ main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#pragma warning(disable: 4100)
+#pragma warning(disable: 4152)
+#pragma warning(disable: 4201)
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#define RGBQUAD _RGBQUAD
+#include <windows.h>
+#undef RGBQUAD
+#include <mmreg.h>
+
+
+#include "audio.h"
